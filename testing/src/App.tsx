a1e9374dc5445eaacf48bcaf61720d1a6ff53cec
sporlichka <tvooybatya@gmail.com>
2025-06-02 21:14:09 +0500

    redeploy

--- testing/src/App.tsx
+++ testing/src/App.tsx
@@ -2,13 +2,11 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Home from './page/Home'
-import Props from './page/Props'
 function App() {
  
   return (
         <Routes>
           <Route path="/" element={<Home/>}></Route>
-          <Route path="/Props" element={<Props/>}></Route>
         </Routes>
   )
 }
